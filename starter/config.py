a5306de1917a7746c7e4f17c70d9d55fb60391dc
Antony Chiu <tony10100476@gmail.com>
2021-09-25 19:11:01 +0800

    db uri revised

--- starter/config.py
+++ starter/config.py
@@ -8,7 +8,7 @@ DB_USER = os.getenv('DB_USER','postgres')
 DB_PASSWORD = os.getenv('DB_PASSWORD','tony55637')
 DB_NAME = os.getenv('DB_NAME', 'capstone_movie')
 # DB_PATH = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
-DB_PATH = 'postgres://soonxhmzrddqnx:8a2dca8b7741fce23d4d5311c2dce5273a6308ef105ae3bd6056b4b8027c77c8@ec2-34-233-187-36.compute-1.amazonaws.com:5432/df1lf4nk1ose1m'
+DB_PATH = 'postgresql://soonxhmzrddqnx:8a2dca8b7741fce23d4d5311c2dce5273a6308ef105ae3bd6056b4b8027c77c8@ec2-34-233-187-36.compute-1.amazonaws.com:5432/df1lf4nk1ose1m'
 DB_NAME_TEST = os.getenv('DB_NAME_TEST', 'capstone_movie_test')
 DB_PATH_TEST = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME_TEST}'
 
